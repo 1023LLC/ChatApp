34cb58a4b2524fabe1237e8a2066dad822757669
julius <nyerereoffice@gmail.com>
2024-06-22 15:15:55 +0300

    Configuring Default Authentication Classes

--- djchat/settings.py
+++ djchat/settings.py
@@ -127,7 +127,10 @@ AUTH_USER_MODEL = "account.Account"
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES" : [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
 
 
