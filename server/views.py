1162ab7189b44cbc88e0f282acce02081e2038ac
julius <nyerereoffice@gmail.com>
2024-07-01 06:28:56 +0300

    Endpoint for Filtering Servers by Server ID

--- server/views.py
+++ server/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from .models import Server
 from .serializers import ServerSerializer
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
+
 
 class ServerListViewSet(viewsets.ViewSet):
     
@@ -12,6 +14,11 @@ class ServerListViewSet(viewsets.ViewSet):
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
+        by_serverid = request.query_params.get("by_serverid")
+        
+        
+        if by_user and not  request.user.is_authenticated:
+            raise AuthenticationFailed()
         
         if category:
             self.queryset = self.queryset.filter(category__name=category)
@@ -22,6 +29,16 @@ class ServerListViewSet(viewsets.ViewSet):
         
         if qty:
             self.queryset = self.queryset[: int(qty)]
+            
+        if by_serverid:
+            try:
+                self.queryset = self.queryset.filter(id=by_serverid)
+                
+                if not self.queryset.exists():
+                    raise ValidationError(detail=f"Server with id {by_serverid} not found")
+                
+            except ValueError:
+                raise ValidationError(detail="Server value error")
             
         
             
